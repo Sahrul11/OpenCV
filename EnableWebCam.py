ba2bad9d8d7dfff36b4904c5cc4d0c8e05b04013
Sahrul <37871766+Sahrul11@users.noreply.github.com>
2020-05-14 13:12:38 +0530

    Add files via upload

---
+++ EnableWebCam.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 12:55:18 2020
+
+@author: MrROBOT
+"""
+
+# import the opencv library 
+import cv2 
+  
+  
+# define a video capture object 
+vid = cv2.VideoCapture(0) 
+  
+while(True): 
+      
+    # Capture the video frame 
+    # by frame 
+    ret, frame = vid.read() 
+  
+    # Display the resulting frame 
+    cv2.imshow('frame', frame) 
+      
+    # the 'q' button is set as the 
+    # quitting button you may use any 
+    # desired button of your choice 
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
+  
+# After the loop release the cap object 
+vid.release() 
+# Destroy all the windows 
+cv2.destroyAllWindows() 
